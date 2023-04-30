a6d3cfa92390289f9944142b51d9cdf0c9e01d30
Akash Patil <akashironman@outlook.com>
2023-04-30 22:36:05 +0530

    Added database connection and migration

---
+++ BookMvcWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using BookMvcWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookMvcWeb.Data;
+public class ApplicationDbContext: DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+    {
+        
+    }
+    public DbSet<Category> Categories { get; set; }
+}
---
+++ BookMvcWeb/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookMvcWeb.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    public int DisplayOrder { get; set; }
+}
